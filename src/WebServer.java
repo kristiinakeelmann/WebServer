291830176ec4b49ec8a56071cb953324e0be80f9
kristiinakeelmann <kristiinake@PC0545.lan>
2019-09-29 21:57:18 +0300

    Move Http reponse creation to method

--- src/WebServer.java
+++ src/WebServer.java
@@ -13,22 +13,40 @@ public class WebServer {
 
         while (true) {
             try (Socket socket = server.accept()) {
-                String httpResponse =
-                        "HTTP/1.1 200 OK\n" +
-                        "Date: Mon, 27 Jul 2009 12:28:53 GMT\n" +
-                        "Server: WebServer\n" +
-                        "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n" +
-                        "Content-Length: 44\n" +
-                        "Content-Type: text/html\n" +
-                        "Connection: Keep-Alive\n\n" +
-                                "<html>" +
-                                "<body>" +
-                                "<h1>It works!</h1>" +
-                                "</body>" +
-                                "</html>";
+                String httpResponseHeader = composeHttpResponseHeader();
+                String httpResponseBody = composeHttpResponseBody();
+                String httpResponse = composeHttpResponse(httpResponseHeader, httpResponseBody);
                 socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
         }
     }
 
+    public static String composeHttpResponseHeader(){
+        var contentLength = composeHttpResponseBody().length();
+        String httpResponseHeader =
+                        "HTTP/1.1 200 OK\n" +
+                        "Date: Mon, 27 Jul 2009 12:28:53 GMT\n" +
+                        "Server: WebServer\n" +
+                        "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n" +
+                        "Content-Length: " + contentLength + "\n" +
+                        "Content-Type: text/html\n" +
+                        "Connection: Keep-Alive\n\n";
+        return httpResponseHeader;
+    }
+
+    public static String composeHttpResponseBody(){
+        var message = "It works!";
+        String httpResponseBody =
+                        "<html>" +
+                        "<body>" +
+                        "<h1>" + message + "</h1>" +
+                        "</body>" +
+                        "</html>";
+        return httpResponseBody;
+    }
+
+    public static String composeHttpResponse(String httpResponseHeader, String httpResponseBody) {
+        String httpResponse = httpResponseHeader + httpResponseBody;
+        return httpResponse;
+    }
 }
